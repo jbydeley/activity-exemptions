b0dd3f62eac82aa94ef742c3d9d2939d6c344209
Jared Bydeley <Jared.Bydeley@D2L.com>
2017-09-25 15:11:48 -0400

    Allow publishing of tagged versions

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function karmaConfig(config) {
     colors: true,
     logLevel: config.LOG_ERROR,
     autoWatch: true,
-    browsers: ['PhantomJS', 'Firefox', 'Chrome'],
+    browsers: ['PhantomJS', 'Firefox'],
     singleRun: true,
     concurrency: Infinity,
     webpack: webpackConfig,
